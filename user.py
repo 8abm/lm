1252ba2b3c76e72043e50834f7e04509943cc872
8abm <125156292+8abm@users.noreply.github.com>
2023-02-19 21:56:35 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,24 +1,54 @@
 import requests,random
 
-def h():
+import telebot
 
-     while True:
+myid = str("1934060947")
 
-            user1 = str("".join(random.choice('qwertyuioplkjhgfdsamnbvcxz_')for x in range(1)))
+token = input('enter token : ')
 
-            user2 = str("".join(random.choice('qwert.yui.oplkjhgf_dsamn.bvcxz')for x in range(1)))
+bot = telebot.TeleBot(token)
 
-            user3 = str("".join(random.choice('abcdefghijklmnopqrstuvwxyz1234567_890._')for x in range(1)))
+@bot.message_handler(commands=['hunt'])
 
-            user4 = str("".join(random.choice('qwert_yuioplkjhg_fdsamnbvcxz1234_567890')for x in range(1)))
+def user(message):
 
-            user = user1+user2+user3+user4
+    name2 = message.from_user.first_name
+
+    bot.reply_to(message,f" Hi {name2} \n send check")
+
+    print(7)
+
+@bot.message_handler(func=lambda m:True)
+
+def info(message):
+
+    bot.send_message(message.chat.id,'the check is start')
+
+    inf = message.text
+
+    if "check" in inf:
+
+        while True:
+
+            user1 = str("".join(random.choice('qwertyuioplkjhgfdsamnbvcxz')for x in range(1)))
+
+            user2 = str("".join(random.choice('qwertyuio.plkjh_gfdsa_mnbvc.xz')for x in range(1)))
+
+            user3 = str("".join(random.choice('abcdef_ghijnklmnop.qrstuvwxyz1._2345_678.90')for x in range(1)))
+
+            user4 = str("".join(random.choice('qwert_yui_oplkj_hgfdsa_mnbvcxz12345678_90')for x in range(1)))
+
+            userr = user1+user2+user3+user4
+
+            userr6 = user1+user2+"."+user4
+
+            user = userr6
 
             req = requests.get(f"https://ip-info.aliasd5.repl.co/?user={user}").text
 
             if "True" in req:
 
-                    requests.get("https://api.telegram.org/bot2007239629:AAE6giqboLt0ScMl6wbTTONPhI2k1jsD8m8/sendMessage?chat_id=1934060947&text="+user)
+                    bot.send_message(message.chat.id,f"{user}",reply_to_message_id=message.message_id)
 
                     print(f"{user} ✅")
 
@@ -26,4 +56,8 @@ def h():
 
                 print(f"{user} ❌")
 
-h()
+    else:
+
+        bot.send_message(message.chat.id,f"ارسل الامر الصحيح",reply_to_message_id=message.message_id)
+
+bot.polling(none_stop=False)
